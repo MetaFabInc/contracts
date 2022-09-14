84ef11fdda592857834a3a0e61e7c5d2a51a21b8
iamarkdev <ark@nftworlds.com>
2022-09-14 00:08:23 -0700

    Lol.

--- deploy.js
+++ deploy.js
@@ -5,7 +5,7 @@ const { JsonRpcProvider } = ethers.providers;
 
 async function main() {
   const provider = new JsonRpcProvider('https://polygon-mainnet.g.alchemy.com/v2/AwxZrUnUtg4-ZdmGtYDLrNcPXI62RXj5');
-  const wallet = new Wallet('0x38e8c46b1f6cec97c16d02d9a159ce327fbd2b2bf6a8b47094fd9522ae93968b', provider);
+  const wallet = new Wallet('TODO', provider);
 
   const ForwarderFactory = await ethers.getContractFactory('ERC2771_Trusted_Forwarder', wallet);
 
