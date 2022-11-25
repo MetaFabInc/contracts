46411ca3e646271a1173cf7235fe760f2d05d124
iamarkdev <ark@nftworlds.com>
2022-11-25 15:54:15 -0800

    Added initial code for lootboxes.

--- contracts/Lootbox/IGame_Lootbox.sol
+++ contracts/Lootbox/IGame_Lootbox.sol
@@ -26,26 +26,41 @@ interface IGame_Lootbox  {
     uint256[] outputCollectionItemAmounts;
     uint256[] outputCollectionItemWeights;
     uint256 outputTotalItems;
+
+    uint256 opens;
     uint256 lastUpdatedAt;
+  }
+
+  struct OpenedLootbox {
+    uint256 lootboxId;
+    uint256 claimSeedBlock;
+    uint256[] outputCollectionItemIds;
+    uint256[] outputCollectionItemAmounts;
+    bool claimed;
   }
 
   // events
   event LootboxOpened(uint256 indexed lootboxId, Lootbox lootbox, address user);
+  event LootboxClaimed(uint256 indexed lootboxId, Lootbox lootbox, address user, uint256 openedLootboxIndex, uint256[] outputCollectionItemIds, uint256[] outputCollectionItemAmounts);
   event LootboxSet(uint256 indexed lootboxId, Lootbox lootbox);
   event LootboxRemoved(uint256 indexed lootboxId, Lootbox lootbox);
 
   // function
   function allLootboxIds() external view returns (uint256[] memory);
-
   function allLootboxes() external view returns (Lootbox[] memory);
+  function allOpenedLootboxes(address _address, uint256 _lootboxId) external view returns (OpenedLootbox[] memory);
   function allLootboxLastUpdates() external view returns (uint256[][] memory);
   function paginateLootboxIds(uint256 _startIndexInclusive, uint256 _limit) external view returns(uint256[] memory);
   function paginateLootboxes(uint256 _startIndexInclusive, uint256 _limit) external view returns(Lootbox[] memory);
+  function paginateOpenedLootboxes(address _address, uint256 _lootboxId, uint256 _startIndexInclusive, uint256 _limit) external view returns (OpenedLootbox[] memory);
   function paginateLootboxLastUpdates(uint256 _startIndexInclusive, uint256 _limit) external view returns(uint256[][] memory);
   function lootboxId(uint256 _index) external view returns (uint256);
   function lootbox(uint256 _lootboxId) external view returns (Lootbox memory);
+  function openedLootbox(address _address, uint256 _lootboxId, uint256 _openedLootboxIndex) external view returns (OpenedLootbox memory);
   function lootboxLastUpdate(uint256 _lootboxId) external view returns (uint256[] memory);
   function totalLootboxes() external view returns (uint256);
+  function totalOpenedLootboxes(address _address, uint256 _lootboxId) external view returns (uint256);
+  function totalClaimableLootboxes(address _address, uint256 _lootboxId) external view returns (uint256);
 
   function setLootbox(
     uint256 _lootboxId,
@@ -53,8 +68,11 @@ interface IGame_Lootbox  {
     uint256[][2] calldata _inputOutputCollectionItemIds,      // 0: inputItemIds, 1: outputItemIds
     uint256[][2] calldata _inputOutputCollectionItemAmounts,  // 0: inputItemAmounts, 1: outputItemAmounts
     uint256[] calldata _outputCollectionItemWeights,
-    uint256 outputTotalItems
+    uint256 _outputTotalItems
   ) external;
+  function openLootbox(uint256 _lootboxId) external;
+  function claimLootbox(uint256 _lootboxId, uint256 _openedLootboxIndex) external;
+  function claimLootboxes(uint256 _lootboxId) external;
   function removeLootbox(uint256 _lootboxId) external;
-  function openLootbox(uint256 _lootboxId) external payable;
+  function setClaimableBlockOffset(uint256 _offset) external;
 }
