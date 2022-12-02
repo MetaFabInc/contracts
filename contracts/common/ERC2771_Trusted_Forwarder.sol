ab84571aabbfa5374be5ea942e8c7d7aeb8c8efb
iamarkdev <ark@nftworlds.com>
2022-12-02 15:43:12 -0800

    Updated trusted forwarder.

--- contracts/common/ERC2771_Trusted_Forwarder.sol
+++ contracts/common/ERC2771_Trusted_Forwarder.sol
@@ -27,7 +27,11 @@ contract ERC2771_Trusted_Forwarder is EIP712 {
   bytes32 private constant _TYPEHASH =
     keccak256("ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data)");
 
+  // mapping from account to gasless tx nonces to prevent replay
   mapping(address => mapping(uint256 => bool)) private _nonces;
+
+  // mapping from account to gasless delegate approvals
+  mapping(address => mapping(address => bool)) private _delegateApprovals;
 
   constructor() EIP712("ERC2771_Trusted_Forwarder", "1.0.0") {}
 
@@ -36,7 +40,7 @@ contract ERC2771_Trusted_Forwarder is EIP712 {
       keccak256(abi.encode(_TYPEHASH, req.from, req.to, req.value, req.gas, req.nonce, keccak256(req.data)))
     ).recover(signature);
 
-    return !_nonces[req.from][req.nonce] && signer == req.from;
+    return !_nonces[req.from][req.nonce] && (signer == req.from || _delegateApprovals[req.from][signer]);
   }
 
   function execute(ForwardRequest calldata req, bytes calldata signature) public payable returns (bool, bytes memory) {
@@ -53,5 +57,18 @@ contract ERC2771_Trusted_Forwarder is EIP712 {
     assert(gasleft() > req.gas / 63);
 
     return (success, returndata);
+  }
+
+  function setApprovalForAll(address delegate, bool approved) external {
+    _delegateApprovals[msg.sender][delegate] = approved;
+  }
+
+  function setApprovalForAllBySignature(address delegate, bool approved, address signer, uint256 nonce, bytes calldata signature) external {
+    address recoveredSigner = keccak256(abi.encode(delegate, approved, signer, nonce)).toEthSignedMessageHash().recover(signature);
+    require(signer == recoveredSigner, "Signer recovered from signature mismatched signer");
+    require(!_nonces[signer][nonce], "nonce has been used");
+
+    _nonces[signer][nonce] = true;
+    _delegateApprovals[signer][delegate] = approved;
   }
 }
---
+++ test/common/ERC2771_Trusted_Forwarder.js
@@ -0,0 +1,42 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+const { BigNumber } = ethers;
+
+const abiCoder = ethers.utils.defaultAbiCoder;
+
+describe('ERC2771_Trusted_Forwarder', () => {
+  let forwarderAddress;
+  let forwarderContract;
+  let owner;
+  let delegate;
+  let otherAddresses;
+
+  beforeEach(async () => {
+    const [ _owner, _delegate, ..._otherAddresses ] = await ethers.getSigners();
+
+    const ERC2771_Trusted_Forwarder = await ethers.getContractFactory('ERC2771_Trusted_Forwarder');
+
+    owner = _owner;
+    delegate = _delegate;
+    otherAddresses = _otherAddresses;
+
+    forwarderContract = await ERC2771_Trusted_Forwarder.deploy();
+    forwarderAddress = forwarderContract.address;
+  });
+
+  /*
+   * General Tests
+   */
+
+  it('Should deploy', async () => {
+    await forwarderContract.deployed();
+  });
+
+  it('Should set delegate approval', async () => {
+    //await forwarderContract.connect(otherAddresses[1]).setApprovalForAll(delegate, true);
+  });
+
+  it('Should set delegate approval by signature', async () => {
+    //
+  });
+});
