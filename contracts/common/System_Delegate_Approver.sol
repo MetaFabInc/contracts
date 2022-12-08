7c304ac2944dc101651dca9c1bc55004fea91046
iamarkdev <ark@nftworlds.com>
2022-12-08 15:57:31 -0800

    Removed unused EIP for system delegate approver. Bumped version

--- contracts/common/System_Delegate_Approver.sol
+++ contracts/common/System_Delegate_Approver.sol
@@ -10,7 +10,6 @@
 pragma solidity ^0.8.16;
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 
 contract System_Delegate_Approver {
   using ECDSA for bytes32;
@@ -19,14 +18,14 @@ contract System_Delegate_Approver {
   mapping(address => mapping(uint256 => bool)) private _nonces;
 
   // mapping from account address -> system id -> delegate signer address approvals
-  mapping(address => mapping(bytes32 => mapping(address => bool))) private _systemDelegateApprover;
+  mapping(address => mapping(bytes32 => mapping(address => bool))) private _systemDelegateApprovals;
 
   function isDelegateApprovedForSystem(address account, bytes32 systemId, address delegate) public view returns (bool) {
-    return _systemDelegateApprover[account][systemId][delegate];
+    return _systemDelegateApprovals[account][systemId][delegate];
   }
 
   function setDelegateApprovalForSystem(bytes32 systemId, address delegate, bool approved) external {
-    _systemDelegateApprover[msg.sender][systemId][delegate] = approved;
+    _systemDelegateApprovals[msg.sender][systemId][delegate] = approved;
   }
 
   function setDelegateApprovalForSystemBySignature(bytes32 systemId, address delegate, bool approved, address signer, uint256 nonce, bytes calldata signature) external {
@@ -35,6 +34,6 @@ contract System_Delegate_Approver {
     require(!_nonces[signer][nonce], "nonce has been used");
 
     _nonces[signer][nonce] = true;
-    _systemDelegateApprover[signer][systemId][delegate] = approved;
+    _systemDelegateApprovals[signer][systemId][delegate] = approved;
   }
 }
